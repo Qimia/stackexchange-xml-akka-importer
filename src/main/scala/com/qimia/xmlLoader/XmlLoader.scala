f108f0a8b9f3f3628b51df690f29c3809c45cf3f
Oguz Uzman <o.uzman@qimia.de>
2019-02-11 11:23:41 +0100

    asd

--- src/main/scala/com/qimia/xmlLoader/XmlLoader.scala
+++ src/main/scala/com/qimia/xmlLoader/XmlLoader.scala
@@ -6,7 +6,6 @@ import akka.actor.{ActorSystem, Props}
 import com.qimia.xmlLoader.actor.{SaveBatchCsvActor, XmlEventReaderActor}
 import com.qimia.xmlLoader.util.{ArgumentParser, Config, FileLoadBalance, StopWordsBloomFilter}
 import akka.routing.RoundRobinPool
-import com.qimia.xmlLoader.actor.XmlEventReaderActor.saveActorName
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
@@ -22,7 +21,7 @@ object XmlLoader {
     val system = ActorSystem("MySystem")
     val readXmlActor = system.actorOf(RoundRobinPool(config.numberOfReadActors).props(Props(new XmlEventReaderActor(config))), name = "readXmlActor")
     val xmlFileList = recursiveListFiles(new File(config.inputPath))
-      .filter(x => x.isFile && x.getAbsolutePath.endsWith("Posts.xml"))//TODO possibiliyty of comments
+      .filter(x => x.isFile && x.getAbsolutePath.endsWith("Posts.xml"))
       .sorted(Ordering.fromLessThan((file: File, file1: File) => file.length()<file1.length()))
       .map(_.getAbsolutePath)
       .zipWithIndex
