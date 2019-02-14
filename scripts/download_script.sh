34c2d958e4f151e4bf3bf269e22d0c72ea35d035
Zohair Aashiq <z.aashiq@qimia.de>
2019-02-14 14:11:42 +0100

    Download scripts for downloading the torrent files on server

---
+++ scripts/download_script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+CONFIGFILE=../qimtagconfig.ini 
+
+# Check config file
+if [ ! -f $CONFIGFILE ]; then
+   echo "Make sure $CONFIGFILE is in the base directory"
+   exit 1
+fi
+
+# Get and check data directory
+dat_dir=$(awk -F "=" '/data_folder/ {print $2}' $CONFIGFILE | tr -d ' ')
+if [ ! -d $dat_dir ]; then
+   echo "Make sure the data folder: $dat_dir exists"
+   exit 1
+fi
+
+# path of torrent link"
+TORRENT_LINK=https://archive.org/download/stackexchange/stackexchange_archive.torrent
+#script to unzip all file of directory
+BASEDIR=$(dirname $0)
+TARGET_DIR = "$dat_dir/torrent_files"
+# Create directory if it does not exist
+mkdir -p "$TARGET_DIR"
+
+if [ $(dpkg-query -W -f='${Status}' transmission-cli>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install transmission-cli;
+fi
+
+#check weather torrent link exist or not
+if $(wget --spider "$TORRENT_LINK" >/dev/null);
+ then
+	transmission-cli -w "$TARGET_DIR" "$TORRENT_LINK";
+else
+  echo "File does not exist"
+	exit 1
+fi
+
+
